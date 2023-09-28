d361f0ef846211253b4b66f81d69188f1b28a45d
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-09-28 21:13:36 +0300

    Sprint0Task6

---
+++ Tyuiu.VolodinaAA.Csharp.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolodinaAA.Csharp.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolodinaAA.Csharp.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eae0792e-ad3b-4a3e-a1a4-021fe10dced0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VolodinaAA.Csharp.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolodinaAA.Csharp.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.VolodinaAA.Csharp.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма эл-в массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность эл-в массива = " + DataService.SubstractionArray(numsArray));
+            Console.WriteLine("Произведение эл-в массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
