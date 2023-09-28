dd4f82438112c3cb3bed7b12227ba8358e3ad147
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-09-28 18:34:48 +0300

    Sprint0Task4

---
+++ Tyuiu.VolodinaAA.Csharp.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolodinaAA.Csharp.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.VolodinaAA.Csharp.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма = "+DataService.Addition(3, 5));
+            Console.WriteLine("Разность = "+DataService.Substraction(3, 5));
+            Console.WriteLine("Произведение = "+DataService.Multiplication(3, 5));
+            Console.WriteLine("Частное = "+DataService.Division(5,5));
+            Console.ReadKey();
+        }
+    }
+}
