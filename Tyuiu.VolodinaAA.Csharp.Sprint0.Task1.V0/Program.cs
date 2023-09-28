cb41bbcce3fa048caa3c7067a2775893921109cf
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-09-28 15:53:13 +0300

    Sprint0Task1

---
+++ Tyuiu.VolodinaAA.Csharp.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VolodinaAA.Csharp.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Саша";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня звать " + name + " мне " + age + " лет ");
+            Console.ReadKey();
+        }
+    }
+}
