f3f47e3623716c7cbc97f3e021b3ff9f0ce9b22c
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-09-29 18:48:58 +0300

    Sprint0Task7

---
+++ Tyuiu.VolodinaAA.Csharp.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VolodinaAA.Csharp.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne,int[] numTwo)//два одномерных целочисленных массива в качестве аргументов
+        {
+            int[] resultArray = new int[5];//создаем одномерный целочисленный массив на 5 элементов
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
---
+++ Tyuiu.VolodinaAA.Csharp.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolodinaAA.Csharp.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolodinaAA.Csharp.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e993089-45f2-4e70-81d5-19678bdf90af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
